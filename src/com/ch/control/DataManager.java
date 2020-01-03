8911bab8864e8600b627c2e39bea1511bf680d37
caohanandyuci <283190253@qq.com>
2020-01-03 19:33:58 +0800

    Update DataManager.java

--- src/com/ch/control/DataManager.java
+++ src/com/ch/control/DataManager.java
@@ -1,5 +1,6 @@
 package com.ch.control;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.ch.entity.CategoryBean;
@@ -14,16 +15,33 @@ public class DataManager {
 
 	private SparseArray<List<GoodsBean>> mSparseArray = new SparseArray<List<GoodsBean>>();
 	
+	private SparseArray<CategoryBean> mCategoryMap = new SparseArray<CategoryBean>();
+	
 	private void loaddata(Context context){
 		RecordsDatabaseHelper databasehelper = new RecordsDatabaseHelper(context);
 		
 		List<CategoryBean> categoryBeans = databasehelper.queryCategoryBeans();
 		int categorycount = categoryBeans.size();
 		for(CategoryBean categoryBean:categoryBeans){
+			if(categoryBean!=null){
+				int type = categoryBean.getCategoryID();
+				mCategoryMap.put(type, categoryBean);
+			}
+		}
+		List<GoodsBean> goodsBeans = databasehelper.querGoodsBeans();
+		for(GoodsBean goodsbean:goodsBeans){
+			if(goodsbean!=null){
+				int goodscategory = goodsbean.getGoodsCategroy();
+				mSparseArray.put(goodscategory, goodsBeans);
+			}
+		}
+		for(int i=0;i<mCategoryMap.size();i++){
 			
 		}
-		
-		List<GoodsBean> goodsBeans = databasehelper.querGoodsBeans();
-		
+	}
+	
+	public List<GoodsBean> getGoodsBeanByCategory(){
+		List<GoodsBean> goodsBeans = new ArrayList<GoodsBean>();
+		return goodsBeans;
 	}
 }
