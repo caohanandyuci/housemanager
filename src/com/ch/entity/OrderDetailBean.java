2cb826d6add7d456835e9d2f7dc6862707cd0800
caohanandyuci <283190253@qq.com>
2020-01-03 19:32:21 +0800

    Update OrderDetailBean.java

--- src/com/ch/entity/OrderDetailBean.java
+++ src/com/ch/entity/OrderDetailBean.java
@@ -2,6 +2,14 @@ package com.ch.entity;
 
 public class OrderDetailBean {
 
+	public static final String KEY_STRING_GOODID = "goodid";
+	
+	public static final String KEY_STRING_ORDERID = "orderid";
+	
+	public static final String KEY_INT_NUMBER= "number";
+	
+	public static final String KEY_LONG_RECORDTIME = "recordtime";
+	
 	private String mGoodID = "";
 	
 	private String mOrderID = "";
@@ -9,4 +17,38 @@ public class OrderDetailBean {
 	private int mNumber = 0;
 	
 	private long mRecordTime = 0L;
+
+	public String getGoodID() {
+		return mGoodID;
+	}
+
+	public void setGoodID(String goodID) {
+		mGoodID = goodID;
+	}
+
+	public String getOrderID() {
+		return mOrderID;
+	}
+
+	public void setOrderID(String orderID) {
+		mOrderID = orderID;
+	}
+
+	public int getNumber() {
+		return mNumber;
+	}
+
+	public void setNumber(int number) {
+		mNumber = number;
+	}
+
+	public long getRecordTime() {
+		return mRecordTime;
+	}
+
+	public void setRecordTime(long recordTime) {
+		mRecordTime = recordTime;
+	}
+	
+	
 }
