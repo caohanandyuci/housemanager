d9f5955b06741c57157fc1fc423913f7ad6d4bb7
caohanandyuci <283190253@qq.com>
2020-01-03 19:30:38 +0800

    Update CategoryBean.java

--- src/com/ch/entity/CategoryBean.java
+++ src/com/ch/entity/CategoryBean.java
@@ -1,9 +1,42 @@
 package com.ch.entity;
 
 public class CategoryBean {
+	
+	public static final String KEY_STRING_UUID="uuid";
+	
+	public static final String KEY_INT_CATEGORYID = "categoryid";
+	
+	public static final String KEY_STRING_NAME=  "name";
+	
 	private String UUID = "";
 	
 	private int mCategoryID = 0;
 	
 	private String mName = "";
+
+	public String getUUID() {
+		return UUID;
+	}
+
+	public void setUUID(String uUID) {
+		UUID = uUID;
+	}
+
+	public int getCategoryID() {
+		return mCategoryID;
+	}
+
+	public void setCategoryID(int categoryID) {
+		mCategoryID = categoryID;
+	}
+
+	public String getName() {
+		return mName;
+	}
+
+	public void setName(String name) {
+		mName = name;
+	}
+	
+	
 }
