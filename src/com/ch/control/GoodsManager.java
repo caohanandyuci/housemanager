124df81fb05e70dd38057585445e57d739dcbe20
caohanandyuci <283190253@qq.com>
2020-01-17 13:45:02 +0800

    Update GoodsManager.java

--- src/com/ch/control/GoodsManager.java
+++ src/com/ch/control/GoodsManager.java
@@ -13,4 +13,45 @@ public class GoodsManager {
 	public void LoadGoodsBean(List<GoodsBean> goodsbean){
 		mGoodsBeans.addAll(goodsbean);
 	}
+	
+	public void UpdateGoodsBean(List<GoodsBean> goodsbean){
+		mGoodsBeans.clear();
+		mGoodsBeans.addAll(goodsbean);
+	}
+	
+	public void ClearGoodsBeans(){
+		mGoodsBeans.clear();
+	}
+	
+	public GoodsBean findGoodsBeanByID(String goodsid){
+		for(GoodsBean goodsbean: mGoodsBeans){
+			if(goodsbean!=null && goodsbean.getGoodsID()!=null){
+				if(goodsbean.getGoodsID().equals(goodsid)){
+					return goodsbean;
+				}
+			}
+		}
+		return null;
+	}
+	
+	public GoodsBean findGoodsBeanByName(String name){
+		for(GoodsBean goodsbean: mGoodsBeans){
+			if(goodsbean!=null && goodsbean.getGoodsName()!=null){
+				if(goodsbean.getGoodsName().equals(name)){
+					return goodsbean;
+				}
+			}
+		}
+		return null;
+	}
+	
+	public boolean hasGoodsName(GoodsBean goodsbean){
+		for(GoodsBean goods:mGoodsBeans){
+			if(goods!=null && goods.isEquals(goodsbean)){
+				return true;
+			}
+		}
+		return false;
+	}
+	
 }
